d2bdb66007b6eb0f1fed30c4e2e35b884e341a2b
Omar Khaled <omar.khaled1878@gmail.com>
2024-11-05 22:12:06 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+// const montserrat = localFont({
+//   src: "./fonts/Montserrat-VariableFont_wght.ttf",
+//   variable: "--font-montserrat",
+//   weight: "100 900",
+// });
+const Arial = localFont({
+  src: "./fonts/Arial.ttf",
+  variable: "--font-Arial",
+});
+
+export const metadata: Metadata = {
+  title: "Sky Insurance",
+  description: "Sky Insurance",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={` ${Arial.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
